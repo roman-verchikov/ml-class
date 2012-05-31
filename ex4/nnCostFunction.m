87a0fe478fc779d91a06f8aa54e0ef56aacfaaff
Roman Verchikov <roman.verchikov@gmail.com>
2012-05-31 16:38:14 +0300

    Implemented exercise 4 (100/100)

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -62,25 +62,48 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% -------------------------------------------------------------
+% Part 1 - Feedforward
+a1 = [ones(m,1) X];
+z2 = Theta1 * a1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m,1) a2'];
+z3 = Theta2 * a2';
+a3 = sigmoid(z3);
+
+% unroll y, e.g. y = [2;4] -> [ [0 1 0 0]; [0 0 0 1] ]; where K = 4
+K = size(a3,1);
+for i = 1:size(y,1)
+    y_mod(i,:) = [zeros(1, y(i,1)-1) 1 zeros(1,K-y(i,1))];
+end
+
+a = -y_mod' .* log(a3);
+b = (1-y_mod)' .* log(1-a3);
+
+% -------------------------------------------------------------
+% Part 2 - Regularized cost function
+% regularization term
+unbiasedTheta1 = Theta1(:,2:end);
+unbiasedTheta2 = Theta2(:,2:end);
+sumOfSquaredThetas = sum(sum(unbiasedTheta1.^2)) + sum(sum(unbiasedTheta2.^2));
+regTerm = lambda * sumOfSquaredThetas / (2*m);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% regularized cost function:
+J = sum(sum(a - b)/m) + regTerm;
 
 
 % -------------------------------------------------------------
+% Part 3 - Backpropagation
+
+delta3 = a3' - y_mod;
+delta2 = unbiasedTheta2' * delta3' .* sigmoidGradient(z2);
+
+Theta1(:,1) = 0;
+Theta2(:,1) = 0;
+Theta1_grad = (delta2 * a1 + lambda * Theta1)/m;
+Theta2_grad = (delta3' * a2 + lambda * Theta2)/m;
 
 % =========================================================================
 
